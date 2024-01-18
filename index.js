37e06537c43733757737683bc985cb2ae33e5a20
Brian <brianmariga0@gmail.com>
2024-01-18 19:12:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const numbers = {};
+
+  for (const num of array) {
+    const numbers2 = target - num;
+    if (numbers2 in numbers) {
+      return true;
+    }
+    numbers[num] = true;
+  }
+
+  return false;
 }
 
 /* 
@@ -8,10 +19,18 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+1. Initialize an empty object called 'numbers'.
+2. Loop through each element 'num' in the input array.
+3. Calculate 'numbers2' as the target minus 'num'.
+4. If 'numbers2' is in 'numbers', return true (a pair is found).
+5. Add 'num' to 'numbers'.
+6. If the loop completes without finding a pair, return false.
+
 */
 
 /*
   Add written explanation of your solution here
+  The function uses an object) called numbers to keep track of the numbers encountered during the iteration. For each element num in the array, it calculates numbers2 as the difference between the target and num. If numbers2 is already in numbers, it means a pair is found that adds up to the target, and the function returns true. Otherwise, num is added to numbers.
 */
 
 // You can run `node index.js` to view these console logs
